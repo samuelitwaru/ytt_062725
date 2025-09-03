6209cdb301c4694375f632574ae65207a948f94a
samuelitwaru <samuelitwaru@gmail.com>
2025-09-03 12:58:26 +0300

    more

--- SoapParm.cs
+++ SoapParm.cs
@@ -5,11 +5,6 @@ using GeneXus.Resources;
 using GeneXus.Application;
 using GeneXus.Metadata;
 using GeneXus.Cryptography;
-using GeneXus.Reorg;
-using System.Threading;
-using GeneXus.Programs;
-using System.Data;
-using GeneXus.Data;
 using GeneXus.Data.ADO;
 using GeneXus.Data.NTier;
 using GeneXus.Data.NTier.ADO;
@@ -18,6 +13,7 @@ using GeneXus.Search;
 using GeneXus.Encryption;
 using GeneXus.Http.Client;
 using System.Xml.Serialization;
+using System.Runtime.Serialization;
 namespace GeneXus.Programs {
    public class SoapParm
    {
